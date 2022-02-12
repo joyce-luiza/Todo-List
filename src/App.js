de218ab06b2bdb5ea47b6967bbf7a787955eb8e3
joyce-luiza <joyceluizabrito@gmail.com>
2022-02-12 17:40:23 -0300

    adding todo form

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
+import { useState } from "react";
 import "./App.css";
+import Form from "./components/Form";
 
 function App() {
+    const [inputText, setInputText] = useState("");
+    const [todos, setTodos] = useState([]);
+
     return (
         <div className="App">
-            <h1>Hello world!</h1>
+            <h1>Todo List</h1>
+            <Form
+                todos={todos}
+                setTodos={setTodos}
+                inputText={inputText}
+                setInputText={setInputText}
+            />
         </div>
     );
 }
