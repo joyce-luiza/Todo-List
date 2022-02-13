44b60a8624b421648c23c0a6bbf7bd53fd90022a
joyce-luiza <joyceluizabrito@gmail.com>
2022-02-13 11:49:20 -0300

    adding delete function

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,11 +1,16 @@
 import React from "react";
 
-const Todo = ({ text }) => {
+const Todo = ({ text, todo, todos, setTodos }) => {
+    // Delete todo
+    const deleteHandler = () => {
+        setTodos(todos.filter((item) => item.id !== todo.id));
+    };
+
     return (
         <div className="todo">
             <input type="checkbox" />
             <li className={`todo-item`}>{text}</li>
-            <button className="trash-btn">
+            <button className="trash-btn" onClick={deleteHandler}>
                 <i className="ri-delete-bin-7-line"></i>
             </button>
         </div>
