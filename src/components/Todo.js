50a88ed29f3af6d3d520ae6c4393fb95966afa93
joyce-luiza <joyceluizabrito@gmail.com>
2022-02-13 12:05:13 -0300

    adding complete todo function

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -6,10 +6,26 @@ const Todo = ({ text, todo, todos, setTodos }) => {
         setTodos(todos.filter((item) => item.id !== todo.id));
     };
 
+    const completeHandler = () => {
+        setTodos(
+            todos.map((item) => {
+                if (item.id === todo.id) {
+                    return {
+                        ...item,
+                        completed: !item.completed,
+                    };
+                }
+                return item;
+            })
+        );
+    };
+
     return (
         <div className="todo">
-            <input type="checkbox" />
-            <li className={`todo-item`}>{text}</li>
+            <input type="checkbox" onClick={completeHandler} />
+            <li className={`todo-item ${todo.completed ? "completed" : ""}`}>
+                {text}
+            </li>
             <button className="trash-btn" onClick={deleteHandler}>
                 <i className="ri-delete-bin-7-line"></i>
             </button>
